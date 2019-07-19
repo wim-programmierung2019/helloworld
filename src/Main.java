d857349d087e3ae1b6813abe0aaf7ef938fb486b
ronjalinnenbrink <ronja.linnenbrink@claas.com>
2019-07-19 09:54:32 +0000

    keine Änderung

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    /**
+     * Einstieg in ein Javaprogramm.
+     */
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+    }
+    
+}
