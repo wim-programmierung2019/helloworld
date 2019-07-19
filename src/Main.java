34640960bb6cbc8f57df40ecca3b617edac84dd3
rybalt <arybaltschenko@web.de>
2019-07-19 10:10:34 +0000

    Test

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
      * Einstieg in ein Javaprogramm.
      */
     public static void main(String[] args){
-        System.out.println("Hello, mannn 2!");
+        System.out.println("Hello, Test!");
     }
     
 }
